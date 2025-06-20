19b019d2106f9fe4cd9f12d0a14429f54082f8dd
Sahana-DS <sahana2000ds@gmail.com>
2025-06-20 10:18:57 +0530

    Sahana-Assets-Module

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Middleware
 app.use(cors());
@@ -11,9 +11,9 @@ app.use(express.json());
 // PostgreSQL connection configuration
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'asset_management',
-    password: 'root',
+    password: 'admin234',
     port: 5432,
 });
 
@@ -187,5 +187,5 @@ app.get('/api/stats', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://51.20.252.164:${port}`);
 });
